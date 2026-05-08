e29a10aacafcbd3eaf781db390096ca05f9a6e08
JustJaimii <jancarlodalacat@gmail.com>
2026-05-08 12:10:20 +0800

    Initial commit

---
+++ src/main/java/project/kompass/arrowPlus/ArrowPlus.java
@@ -0,0 +1,17 @@
+package project.kompass.arrowPlus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArrowPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
